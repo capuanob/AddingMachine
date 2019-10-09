b04da7031a010c53eb695347376351c5d42a49a3
capuanob <bcapuano@asu.edu>
2019-10-08 17:17:24 -0700

    Initial commit

---
+++ assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,71 @@
+/**
+ * @author Bailey Capuano
+ * ClassID: 85141
+ * Assignment Number: 2
+ * This file implements the AddingMachine class. This class provides the functionality required to keep track of a 
+ * running sum of additions and subtractions. 
+ *
+ */
+
+package cse360assign2;
+
+public class AddingMachine {
+	
+	/**
+	 * Holds the sum of the additions and subtractions within the AddingMachine
+	 */
+	private int total;
+	
+	/**
+	 * Default class constructor, sets total to a default value of zero.
+	 */
+	public AddingMachine() {
+		total = 0; // not needed - included for clarity
+	}
+	
+	/**
+	 * Gets the running total of the AddingMachine
+	 * 
+	 * @return value of total	
+	 */
+	public int getTotal() {
+		return 0;
+	}
+	
+	
+	/**
+	 * Adds to the AddingMachine's total
+	 * 
+	 * @param value	what to add to the total
+	 */
+	public void add(int value) {
+		
+	}
+	
+	/**
+	 * Subtracts from the AddingMachine's total
+	 * 
+	 * @param value	what to subtract from the total
+	 */
+	public void subtract(int value) {
+		
+	}
+	
+	/*
+	 * Override of Java's toString method, used for easy representation of an AddingMachine for debugging.
+	 * 
+	 * @return	a string representation of the AddingMachine object.
+	 */
+	public String toString() {
+		return "";
+	}
+	
+	/**
+	 * Resets the AddingMachine's properties.
+	 */
+	public void clear() {
+		
+	}
+	
+	
+}
